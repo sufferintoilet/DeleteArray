5ed008a14da973eb1924fd5dbc3956cf49519ffa
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-29 23:01:17 +0900

    1

--- Program.cs
+++ Program.cs
@@ -2,27 +2,18 @@
 {
     private static void Main(string[] args)
     {
-       int[] array = {1,2,3,4,5,6,7,8,9};
-       Console.WriteLine("Nhap gia tri can xoa");
-       int x = int.Parse(Console.ReadLine());
-       int n=0;
-       for (int i =0; i < array.Length;i++)
-       {
-        if(array[i]==x)
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        Console.WriteLine("nhap gia tri muon xoa");
+        int n = int.Parse(Console.ReadLine());
+        for(int i=n;i<array.Length-1;i++)
         {
-            n=i;
+            array[i]=array[i+1];
         }
-       }
-       for (int i= n; i< array.Length-1; i++)
+        Array.Resize(ref array,array.Length-1);
+        for(int i=0;i<array.Length;i++)
         {
-            array[i] = array[i+1];
+            Console.Write(array[i]);
+            Console.Write(" ");
         }
-       Console.WriteLine("Mang noi la");
-       for (int i = 0; 1 < array.Length; i++)
-       {
-        Console.WriteLine(array[1]);
-        Console.Write(" ");
-       }
-
     }
 }
